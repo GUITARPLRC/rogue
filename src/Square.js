861adfb40bbff0f02826c8eb9a8da164200064b2
Owner <Owner>
2017-08-05 11:33:26 -0500

    add style change for player view

--- src/Square.js
+++ src/Square.js
@@ -5,9 +5,28 @@ import './styles/Square.css';
 class Square extends Component {
 	render() {
 		const { coords, className, player } = this.props;
-		if (coords.x === player.coords.x && coords.y )
+		let name = className;
+		if (coords.x === player.coords.x && coords.y === player.coords.y) {
+			name = 'square player'
+		} else if ( coords.x === player.coords.x - 1 && coords.y === player.coords.y - 1 ) {
+			name = 'square'
+		} else if ( coords.x === player.coords.x - 1 && coords.y === player.coords.y ) {
+			name = 'square'
+		} else if ( coords.x === player.coords.x - 1 && coords.y === player.coords.y + 1 ) {
+			name = 'square'
+		} else if ( coords.x === player.coords.x && coords.y === player.coords.y - 1 ) {
+			name = 'square'
+		} else if ( coords.x === player.coords.x && coords.y === player.coords.y + 1 ) {
+			name = 'square'
+		} else if ( coords.x === player.coords.x + 1 && coords.y === player.coords.y - 1 ) {
+			name = 'square'
+		} else if ( coords.x === player.coords.x + 1 && coords.y === player.coords.y) {
+			name = 'square'
+		} else if ( coords.x === player.coords.x + 1 && coords.y === player.coords.y + 1) {
+			name = 'square'
+		}
 		return (
-			<div className={className} coords={coords}>
+			<div className={name} coords={coords}>
 
 			</div>
 		)
